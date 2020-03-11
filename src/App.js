52c9c0749665e3e3b210774c5d1170e95218949b
unknown <khoacoyotes.na@gmail.com>
2020-03-11 18:04:21 +0700

    add existing files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
- import React, { Component } from 'react'
  
  export class App extends Component {
   state = {
-    name: 'satori_tech'
+    name: 'hello'
   }
-
-
+  changeName = (newName) =>{
+    this.setState({
+      name: newName
+    })
+  }
+  changeNameFromInput = (event) =>{
+    this.setState({
+      name: event.target.value
+    })
+  }
    render() {
      return (
        <div className="App">
-         
+         <br></br>
+         <button onClick={() => this.changeName('howdy cowboy!')}>Change state using anonymous func</button>
+         <button onClick={(this.changeName.bind(this,'tada...'))}>Change state using bind</button>
+         <br></br>
+          <input type="text" onChange={this.changeNameFromInput} value={this.state.name}></input>
+         <br></br>
+         <div>{this.state.name}</div>
        </div>
      )
    }
