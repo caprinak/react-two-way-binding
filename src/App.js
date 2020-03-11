7e0668b3977a957d8d46c1c37aa1400b0ed220a8
unknown <khoacoyotes.na@gmail.com>
2020-03-11 16:54:33 +0700

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+ import React, { Component } from 'react'
+ 
+ export class App extends Component {
+  state = {
+    name: 'satori_tech'
+  }
+
+
+   render() {
+     return (
+       <div className="App">
+         
+       </div>
+     )
+   }
+ }
+ 
+ export default App
+ 
